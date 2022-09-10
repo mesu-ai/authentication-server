8175b292a21088471e46fa2af96c1ce9add10d4a
Md Ibrahim Adham Mesu <ibrahim.mesu@gmail.com>
2022-09-10 13:42:41 +0600

    update index

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
 const express = require('express');
 const app = express();
-const cors=require('cors');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 
 // middlewire
 app.use(cors());
 app.use(express.json());
 
-
-const uri = "mongodb+srv://authenticationDB:0vhLrCZWczLL3RMs@cluster0.mtxga.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+const { query } = require('express');
+const uri =
+	'mongodb+srv://authenticationDB:0vhLrCZWczLL3RMs@cluster0.mtxga.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-  try {
-    
-    await client.connect();
-    const database = client.db("authenticate");
-    const userCollection = database.collection("user");
-   
+	try {
+		await client.connect();
+		const database = client.db('authenticate');
+		const usersCollection = database.collection('users');
 
-    console.log('connect to db');
-    console.log("Connected successfully to server");
+		app.get('/user', async (req, res) => {
+			const email = req.query.email;
+			const password = req.query.password;
+			console.log(email,password);
+      console.log(req.body);
+			if (email) {
+				const query = { email: email };
+				const result = await usersCollection.findOne(query);
+				console.log(result);
+				res.send(result);
+			}
+			else{
+			  const result = await usersCollection.find({}).toArray();
+        res.send(result);
+        
 
+			}
+		});
 
-  } finally {
-   
-  //  await client.close();
-  }
+		app.post('/user', async (req, res) => {
+      const cursor = req.body;
+      console.log(cursor);
+			const result = await usersCollection.insertOne(cursor);
+			res.json(result);
+		});
+
+		console.log('connect to db');
+		console.log('Connected successfully to server');
+	} finally {
+		//  await client.close();
+	}
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+	res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
